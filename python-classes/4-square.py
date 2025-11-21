d5e8da02bbb6e307dd441e5f99140746afcd8132
kali <kali@kali.kali>
2025-11-21 04:38:23 -0500

    1

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -28,3 +28,10 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
+        """Print the square using the # character."""
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
+
