11c60c6cde40b722cfe6a9404ce19672d6cc0223
kali <kali@kali.kali>
2025-11-21 04:35:49 -0500

    1

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a Square class with size validation and area computation."""
+"""This module defines a Square class with size validation and area method."""
 
 
 class Square:
