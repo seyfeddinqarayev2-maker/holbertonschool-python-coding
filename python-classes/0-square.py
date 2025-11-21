b8751ba7ceae33d825bd7341965d999ac2eabe43
kali <kali@kali.kali>
2025-11-21 04:33:09 -0500

    1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,8 +4,8 @@
 
 class Square:
     """Represents a square with a private size attribute."""
-   
-     def __init__(self, size=0):
+
+    def __init__(self, size=0):
         """Initialize a new Square with a given size (default 0)."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
